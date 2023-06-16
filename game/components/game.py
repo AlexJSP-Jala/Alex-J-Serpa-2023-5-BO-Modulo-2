8c0d9a3a997a26451927f3219947ee3efbb862f0
Alex J Serpa <alex.serpa@fundacion-jala.org>
2023-06-16 16:12:43 -0500

    Homework 4

--- game/components/game.py
+++ game/components/game.py
@@ -1,10 +1,12 @@
 import pygame
+import random
+import sys
 
-from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE, BULLET, BULLET_ENEMY
 
 from game.components.spaceship import SpaceShip
 from game.components.enemy_spaceship import Enemy
-from game.components.bullet import Bullet, bullets
+from game.components.bullet import Bullet
 
 
 
@@ -21,14 +23,30 @@ class Game:
         self.game_speed = 10
         self.x_pos_bg = 0
         self.y_pos_bg = 0
-
         # Game tiene un "Spaceship"
         self.spaceship = SpaceShip()
         #Game has a enemy 
         self.enemy = Enemy(500, 50)
+        self.enemy_2 = Enemy(400, 50)
         # bullet of the spaceship
-        #self.bullet = Bullet(self.spaceship.image_rect.centerx, self.spaceship.image_rect.y )
-        
+        self.bullet_spaceship = Bullet(self.spaceship.image_rect.centerx, self.spaceship.image_rect.y, BULLET)
+        self.bullet_enemy = Bullet(self.enemy.image_rect.centerx, self.enemy.image_rect.bottom, BULLET_ENEMY)
+        #enemy firing frequency
+        self.enemy_firing_frequency = None
+        #variables Game Over
+        self.game_over = False
+        self.restart_status = False
+        self.deaths = 0
+        self.impacts = 0
+        # Text font
+        self.font = pygame.font.Font(None, 36)
+        #Color
+        self.WHITE = (255, 255, 255)
+        self.RED = (255, 0, 0)
+        self.game_over_text = None
+        self.restart_text = None
+        self.deaths_text = None 
+        self.restart = False
 
     def run(self):
         # Game loop: events - update - draw
@@ -36,23 +54,33 @@ class Game:
 
         # while self.playing == True
         while self.playing: # Mientras el atributo playing (self.playing) sea true "repito"
+            
             self.handle_events()
             self.update()
             self.draw()
-            #self.spaceship_movements()
+            if self.game_over:
+                self.game_over_screen()
+            #pygame.display.update()
             
+            #self.game_over(self.game_over_status)
         else:
             print("Something ocurred to quit the game!!!")
-        pygame.display.quit()
-        pygame.quit()
+
+
+        
+            
+            
+            
 
     def handle_events(self):
-        
         # Para un "event" (es un elemento) en la lista (secuencia) que me retorna el metodo get()
+        
         for event in pygame.event.get():
             # si el "event" type es igual a pygame.QUIT entonces cambiamos playing a False
             if event.type == pygame.QUIT:
                 self.playing = False
+                pygame.quit()
+                sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     self.spaceship.move_left = True
@@ -63,8 +91,13 @@ class Game:
                 elif event.key == pygame.K_DOWN:  # Nueva tecla agregada: flecha hacia abajo
                     self.spaceship.move_down = True
                 elif event.key == pygame.K_x:
-                    bullet = Bullet(self.spaceship.image_rect.centerx, self.spaceship.image_rect.y)
-                    bullets.append(bullet)
+                    #bullet = Bullet(self.spaceship.image_rect.centerx, self.spaceship.image_rect.y)
+                    #bullets.append(bullet)
+                    #self.shoot(self.spaceship.image_rect.centerx, self.spaceship.image_rect.y)
+                    self.spaceship.shoot(self.spaceship.image_rect.centerx, self.spaceship.image_rect.y)
+                if event.key == pygame.K_r:
+                    self.restart = True
+                    self.game_over = False
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     self.spaceship.move_left = False
@@ -79,40 +112,94 @@ class Game:
     def update(self):
         # pass
         self.spaceship.update()
+        self.enemy.update()
+        self.enemy_2.update()
+        self.bullet_spaceship.shoot_eliminate(self.enemy, self.spaceship.bullets, 10, 1)
+        self.bullet_spaceship.shoot_eliminate(self.enemy_2, self.spaceship.bullets, 10, 1)
+        self.shoots_enemys(self.enemy)
+        self.bullet_enemy.shoot_eliminate(self.spaceship, self.enemy.bullets, -20, 5)
         
+        #if self.spaceship.image_rect is None:
+        #    self.game_over = True
 
-        for bullet in bullets:
+        print("esto es la variable game", self.game_over)
+        
+        
+    def shoots_enemys(self, variable_enemy):
+        self.enemy_firing_frequency = random.randint(0, 100)
+        if variable_enemy.image_rect is not None and self.enemy_firing_frequency < 50:
+            self.enemy.shoot(self.enemy.image_rect.centerx, self.enemy.image_rect.bottom)
+            print(self.enemy_firing_frequency)
+            # Comprobar colisión con la bala 
+                
+        else:
+            #print("Continue")
+            pass
+    
+
+        """ for bullet in bullets:
             bullet.update()
-            if bullet.image_rect.bottom <= 0: 
+            if bullet.image_rect.top <= 0: 
                 bullets.remove(bullet)
         
         for bullet in bullets:
             if self.enemy.image_rect is not None and bullet.image_rect.colliderect(self.enemy.image_rect):
                 bullets.remove(bullet)
                 self.enemy.image_rect = None
-
-        self.enemy.update()
+            elif self.enemy_2.image_rect is not None and bullet.image_rect.colliderect(self.enemy_2.image_rect):
+                bullets.remove(bullet)
+                self.enemy_2.image_rect = None """
         
+    def game_over_screen(self):
+        
+        self.game_over_text = self.font.render("Game Over", True, self.RED)
+        self.restart_text = self.font.render("Presiona 'R' para reiniciar", True, self.RED)
+        self.deaths_text = self.font.render("Death Count: " + str(self.deaths), True, self.RED)
+        self.screen.fill(self.WHITE)
+        self.screen.blit(self.game_over_text, (SCREEN_WIDTH // 2 - self.game_over_text.get_width() // 2, 200))
+        self.screen.blit(self.restart_text, (SCREEN_WIDTH // 2 - self.restart_text.get_width() // 2, 250))
+        self.screen.blit(self.deaths_text, (SCREEN_WIDTH // 2 - self.deaths_text.get_width() // 2, 300))
+        pygame.display.flip()
+        
+            
+        
+    
+    def restart_game(self):
+        self.restart = False
+        self.deaths = 0
+        self.impac = 0
+        self.spaceship.bullets.clear()
+        self.enemy.bullets.clear()
+        self.spaceship.reset()
+        
+            
 
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
-        
+
 
 
         # dibujamos el objeto en pantalla
-        self.screen.blit(self.spaceship.image, (self.spaceship.image_rect.x, self.spaceship.image_rect.y))
+        if self.spaceship.image_rect is not None:
+            self.screen.blit(self.spaceship.image, (self.spaceship.image_rect.x, self.spaceship.image_rect.y))
+
         # display the enemy spaceship in its current position 
         if self.enemy.image_rect is not None:
             self.screen.blit(self.enemy.image, self.enemy.image_rect)
+        if self.enemy_2.image_rect is not None:
+                self.screen.blit(self.enemy_2.image, self.enemy_2.image_rect)
+        
         # show the positions of the bullets 
-        for bullet in bullets:
+        for bullet in self.spaceship.bullets:
             self.screen.blit(bullet.image, bullet.image_rect)
+        for bullet in self.enemy.bullets:
+            self.screen.blit(bullet.image, bullet.image_rect)
+        
+        
             
-
-
         pygame.display.update()
         pygame.display.flip()
 
